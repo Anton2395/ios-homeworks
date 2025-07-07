63eb4c6c7bf8863e5a9a946a688b21ddbdd86883
Anton Shilin <toha2395@gmail.com>
2025-07-07 21:33:55 +0300

    delited main storyboard, changed params for project without storyboard, added tab bar controller and navigation controllers, added tab bar items

--- Navigation/SceneDelegate.swift
+++ Navigation/SceneDelegate.swift
@@ -13,10 +13,30 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: scene)
+        
+        let tabBarController = UITabBarController()
+        
+        let feedNavigationControlle = UINavigationController()
+        let profileNavigationController = UINavigationController()
+        
+        feedNavigationControlle.tabBarItem = UITabBarItem(
+            title: "Лента",
+            image: UIImage(systemName: "house"),
+            selectedImage: UIImage(systemName: "house.fill")
+        )
+        profileNavigationController.tabBarItem = UITabBarItem(
+            title: "Профиль",
+            image: UIImage(systemName: "person"),
+            selectedImage: UIImage(systemName: "person.fill")
+        )
+        
+        
+    
+        window.makeKeyAndVisible()
+        
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
