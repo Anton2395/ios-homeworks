0855adf745e30894cef79713737375ba965c6a98
Anton Shilin <toha2395@gmail.com>
2025-07-20 16:20:05 +0300

    changed to auto layout, added new button for profile controller

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -11,8 +11,17 @@ class ProfileViewController: UIViewController {
     
     private lazy var profileHeaderView: ProfileHeaderView = {
         let profileView = ProfileHeaderView()
-        
+        profileView.translatesAutoresizingMaskIntoConstraints = false
         return profileView
+    }()
+    
+    
+    private lazy var newAdditionalButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Additional button", for: .normal)
+        button.backgroundColor = .systemMint
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
     
     override func viewDidLoad() {
@@ -20,11 +29,26 @@ class ProfileViewController: UIViewController {
         title = "Profile"
         view.backgroundColor = .lightGray
         view.addSubview(profileHeaderView)
+        view.addSubview(newAdditionalButton)
+        
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            profileHeaderView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 0),
+            profileHeaderView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: 0),
+            profileHeaderView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 0),
+            profileHeaderView.heightAnchor.constraint(equalToConstant: 220),
+            
+            newAdditionalButton.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: 0),
+            newAdditionalButton.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 0),
+            newAdditionalButton.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: 0),
+            newAdditionalButton.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
     }
     
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        
-        profileHeaderView.frame = view.frame
-    }
+//    override func viewWillLayoutSubviews() {
+//        super.viewWillLayoutSubviews()
+//        
+//        profileHeaderView.frame = view.frame
+//    }
 }
